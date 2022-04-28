5fe8e3ad46d2d38398cb5f605832760702720be3
Hicham Sahla <hichamsahla91@gmail.com>
2022-04-28 09:42:39 +0200

    Installed EJS played around with routing. tried some things with EJS like including files and installed bootstrap 5

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-const express = require('express')
-const app = express()
+var express = require('express');
+var app = express();
 
-console.log('start');
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
-app.listen(3000)
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+
+// about page
+app.get('/about', function(req, res) {
+  res.render('pages/about');
+});
+
+app.listen(3000);
+console.log('Server is listening on port 8080');
