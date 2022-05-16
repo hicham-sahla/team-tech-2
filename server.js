2c6612ca2f947af026b1992c6caed71e81fbf529
Hicham Sahla <hichamsahla91@gmail.com>
2022-05-16 11:07:39 +0200

    Added port

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+const port = process.env.PORT || 3000;
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // use res.render to load up an ejs view file
 
 // index page
-app.get('/', function(req, res) {
-  res.render('pages/index');
-  
+app.get("/", function (req, res) {
+  res.render("pages/index");
 });
 
 // about page
-app.get('/getting-started', function(req, res) {
-  var tagline = "No programming concept is complete without a cute animal mascot.";
+app.get("/getting-started", function (req, res) {
+  var tagline =
+    "No programming concept is complete without a cute animal mascot.";
 
-  res.render('pages/onboarding', {
-    tagline: tagline
+  res.render("pages/onboarding", {
+    tagline: tagline,
   });
 });
 
-app.use(express.static(__dirname + '/assets'));
+app.use(express.static(__dirname + "/assets"));
 
-app.listen(3000);
-console.log('Server is listening on port 3000');
+app.listen(port);
+console.log("Server is listening on port 3000");
