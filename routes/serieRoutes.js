0638baa7c514c2b1592b8bd85ec9b2e2ca668d3d
xiaonanpols21 <xiaonanpolspols@gmail.com>
2022-06-13 12:18:57 +0200

    routes proberen

--- routes/serieRoutes.js
+++ routes/serieRoutes.js
@@ -3,12 +3,12 @@ const serieController = require('../controllers/serieController');
 
 const router = express.Router();
 
-
 router.get("/series/create", serieController.serie_create_get);
 router.get('/series', serieController.serie_index);
 router.get('/series/details', serieController.serie_details);
 router.get('/series/form-test', serieController.serie_create_test);
 
-
+// Xiao nan heeft dit toegevoegd
+router.get('/home', serieController.serie_home);
 
 module.exports = router; 
