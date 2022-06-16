6ffe2a35ea665b6d9739147edf9fec2d7b89250d
Hicham Sahla <hichamsahla91@gmail.com>
2022-06-16 09:31:12 +0200

    Implemented Session auth with passport.js

--- server.js
+++ server.js
@@ -7,13 +7,37 @@ const mongoose = require("mongoose");
 const connectDB = require("./config/dbConn");
 const userRoutes = require("./routes/userRoutes");
 const serieRoutes = require("./routes/serieRoutes");
+const bcrypt = require('bcrypt');
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+
+const initializePassport = require("./config/passportConfig");
+initializePassport(
+  passport,
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+)
+
+const users = []
 
 // Connect to MongoDB
 connectDB();
 
 // middleware & static files
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride('_method'));
+// TODO bootstrap weghalen
 app.use("/css",express.static(path.join(__dirname, "node_modules/bootstrap/dist/css")));
 app.use(
   "/js",
@@ -23,7 +47,58 @@ app.use(
 // set the view engine to ejs
 app.set("view engine", "ejs");
 
-// user routes
+app.get('/login', (req, res) => {
+    res.render('auth/login.ejs')
+}) 
+app.get('/', checkAuthenticated, (req, res) => {
+  res.render('auth/index.ejs', { name: req.user.name })
+})
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+  res.render('auth/register.ejs')
+})
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    })
+    res.redirect('/login')
+  } catch {
+    res.redirect('/register')
+  }
+})
+
+app.delete('/logout', (req, res) => {
+  req.logOut()
+  res.redirect('auth/login')
+})
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+
+  res.redirect('login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/')
+  }
+  next()
+}
+
+
 app.use(userRoutes);
 app.use(serieRoutes);
 
