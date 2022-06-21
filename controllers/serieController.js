05313e093bdf308daec78346b343f0023d8bc448
Hicham Sahla <hichamsahla91@gmail.com>
2022-06-21 13:08:22 +0200

    Solved the liking feature]

--- controllers/serieController.js
+++ controllers/serieController.js
@@ -2,27 +2,27 @@ const Serie = require("../models/Serie");
 
 const serie_index = (req, res) => {
   Serie.find()
-  .lean()
-  .then((result) => {
-    res.render("series/index", {
-      series: result
+    .lean()
+    .then((result) => {
+      res.render("series/index", {
+        series: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
 };
 
 const serie_details = (req, res) => {
-const id = req.params.id;
-Serie.findById(id)
-  .lean()
-  .then((result) => {
-    res.render("series/details", { series: result});
-  })
-  .catch((err) => {
-    res.status(404).render("pages/404", { title: "Serie niet gevonden" });
-  });
+  const id = req.params.id;
+  Serie.findById(id)
+    .lean()
+    .then((result) => {
+      res.render("series/details", { series: result });
+    })
+    .catch((err) => {
+      res.status(404).render("pages/404", { title: "Serie niet gevonden" });
+    });
 };
 
 const serie_create_get = (req, res) => {
@@ -30,23 +30,17 @@ const serie_create_get = (req, res) => {
 };
 
 const serie_create_post = (req, res) => {
-   const serie = new Serie(req.body);
- 
+  const serie = new Serie(req.body);
+
   serie
     .save()
     .then((result) => {
-      result,
-      res.send("Gelukt");
+      result, res.send("Gelukt");
     })
     .catch((err) => {
       console.log(err);
     });
 };
-
-
-
-
-
 
 module.exports = {
   serie_index,
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -30,6 +30,7 @@ const checkUser = (req, res, next) => {
         next();
       } else {
         let user = await User.findById(decodedToken.id);
+        console.log(user)
         res.locals.user = user;
         next();
       }
