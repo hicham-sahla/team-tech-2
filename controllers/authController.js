d7443c91b580e2ea7c5b6850ae76eb0eb07fdc01
xiaonanpols21 <xiaonanpolspols@gmail.com>
2022-06-21 08:51:59 +0200

    a href login en signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -75,7 +75,7 @@ module.exports.login_post = async (req, res) => {
     res.status(400).json({ errors });
   }
 
-}
+};
 
 module.exports.serie_like_post = (req, res) => {
   const token = req.cookies.jwt;
